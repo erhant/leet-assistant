ae352097d4ef890ce1ee2300f6864753405a1fc3
erhant <erhany96@gmail.com>
2023-12-08 13:17:27 +0300

    bun + elysia + solid backup

--- solid/src/constants/index.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  /** Base URL for the backend. */
-  BASE_URL: import.meta.env.VITE_BASE_URL || "http://localhost:3000/api",
-  /** Welcome message for the chat screen. */
-  WELCOME_MESSAGE: "Welcome! I am your Leet Assistant, how may I help you for this session?",
-} as const;
--- solid/src/routes/api/getSession.ts
+++
@@ -1,8 +0,0 @@
-import { APIEvent, json } from "solid-start";
-import { read } from "~/api/state";
-
-export async function POST({ request }: APIEvent) {
-  const body = await request.json();
-  const session = await read(request, body.sessionId);
-  return json(session);
-}
